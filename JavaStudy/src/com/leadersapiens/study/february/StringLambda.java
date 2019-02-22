1ca67092833252220193e4c5536a8e701bed37e0
Berea96 <kim-kj960712@daum.net>
2019-02-22 18:34:41 +0900

    StringLambda 수정
    리턴값이 void에 매개변수도 없이 출력만하는 람다를 만들어보았다.
    람다에 대해서 감이 온다~

--- JavaStudy/src/com/leadersapiens/study/february/StringLambda.java
+++ JavaStudy/src/com/leadersapiens/study/february/StringLambda.java
@@ -8,6 +8,14 @@ interface StringLengthOver {
     boolean getLengthOver(String str);
 }
 
+interface StringLengthPlus {
+    int getLengthPlus(String str1, String str2);
+}
+
+interface StringPrint {
+    void printString();
+}
+
 public class StringLambda {
 
     public static void getLength(String str, StringLength stringLength) {
@@ -19,7 +27,12 @@ public class StringLambda {
         System.out.println("String Length > 10 ? : " + stringLengthOver.getLengthOver(str));
     }
 
-    
+    public static void getLengthPlus(String str1, String str2, StringLengthPlus stringLengthPlus) {
+        System.out.println("String1 Length : " + str1.length());
+        System.out.println("String2 Length : " + str2.length());
+
+        System.out.println("String Length Plus : " + stringLengthPlus.getLengthPlus(str1, str2));
+    }
 
     public static void main(String[] args) {
         getLength("This is Lambda", (str) -> str.length());
@@ -30,6 +43,12 @@ public class StringLambda {
 
         System.out.println("\n=================================\n");
 
+        getLengthPlus("This is Lambda", "ThisLambda", (str1, str2) -> str1.length() + str2.length());
 
+        System.out.println("\n=================================\n");
+
+        StringPrint stringPrint = () -> System.out.println("헬로우");
+
+        stringPrint.printString();
     }
 }
