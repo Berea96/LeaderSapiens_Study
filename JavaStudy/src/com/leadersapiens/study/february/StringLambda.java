410b5fe07f5aa478eae3ace329d97a4dd8e3338a
Berea96 <kim-kj960712@daum.net>
2019-02-22 18:17:34 +0900

    StringLambda 수정
    현재 String의 길이가 10을 넘어서는지 확인하는 람다 함수를 만들었다.
    조금씩 람다에 감을 잡아가는 중이다.
    
    자만하지 말자, 오만하지 말자, 겸손하여라, 매일 배우자!

--- JavaStudy/src/com/leadersapiens/study/february/StringLambda.java
+++ JavaStudy/src/com/leadersapiens/study/february/StringLambda.java
@@ -4,13 +4,32 @@ interface StringLength {
     int getLength(String str);
 }
 
+interface StringLengthOver {
+    boolean getLengthOver(String str);
+}
+
 public class StringLambda {
 
     public static void getLength(String str, StringLength stringLength) {
+        System.out.println("String의 길이를 구해보는 람다");
         System.out.println("String Length : " + stringLength.getLength(str));
     }
 
+    public static void getLengthOver(String str, StringLengthOver stringLengthOver) {
+        System.out.println("String Length > 10 ? : " + stringLengthOver.getLengthOver(str));
+    }
+
+    
+
     public static void main(String[] args) {
         getLength("This is Lambda", (str) -> str.length());
+
+        System.out.println("\n=================================\n");
+
+        getLengthOver("ThisLambda", (str) -> str.length() > 10);
+
+        System.out.println("\n=================================\n");
+
+
     }
 }
