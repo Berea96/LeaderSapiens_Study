e805343940105a9d27bc4cc5dc067bfb81fb9579
Berea96 <kim-kj960712@daum.net>
2019-02-22 16:47:17 +0900

    Example.java 추가
    일단 인터넷에 있는 예제를 코딩해보았다.
    이 부분은 아직 람다나 함수형 프로그래밍 등
    그 외의 것들과 다르지만 차근차근 진행해야지!

---
+++ JavaStudy/src/com/leadersapiens/study/Example.java
@@ -0,0 +1,33 @@
+package com.leadersapiens.study;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Example {
+    public static void findChicagoImperative(final List<String> cities) {
+        for(String city : cities) {
+            if(city.equals("Chicago")) {
+                System.out.println("Found chicago? : True");
+                return;
+            }
+        }
+    }
+
+    public static void findChicagoImperative2(final List<String> cities) {
+        System.out.println("Found chicago? : " + cities.contains("Chicago"));
+    }
+
+    public static void main(String[] args) {
+        List<String> cities = new ArrayList<>();
+        cities.add("NewYork");
+        cities.add("Chicago");
+        cities.add("Hawaii");
+        List<String> cities2 = new ArrayList<>();
+        cities.add("NewYork");
+        cities.add("California");
+        cities.add("Hawaii");
+
+        findChicagoImperative(cities);
+        findChicagoImperative2(cities2);
+    }
+}
