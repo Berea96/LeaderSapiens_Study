3f0b5aeefaa211a30f27cf3af1da4b255373f9d5
mac <kim-kj960712@daum.net>
2019-02-28 19:26:03 +0900

    수정

--- SpringBoot/telegramBot/src/com/leaderSapiens/TestTelegram.java
+++ SpringBoot/telegramBot/src/com/leaderSapiens/TestTelegram.java
@@ -2,7 +2,6 @@ package com.leaderSapiens;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageCaption;
 import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
@@ -24,45 +23,55 @@ public class TestTelegram extends TelegramLongPollingBot {
     public void onUpdateReceived(Update update) {
 
         if(update.hasMessage() && update.getMessage().hasText()) {
-            String text = update.getMessage().getText();
-            System.out.println(text);
-
-            SendMessage message = new SendMessage()
-                    .setChatId(update.getMessage().getChatId())
-                    .setText(text);
-            InlineKeyboardMarkup markupInline = new InlineKeyboardMarkup();
-
-            List<List<InlineKeyboardButton>> rowsInline = new ArrayList<>();
-            List<InlineKeyboardButton> rowInline = new ArrayList<>();
-
-            rowInline.add(new InlineKeyboardButton().setText("적중").setCallbackData("적중"));
-            rowInline.add(new InlineKeyboardButton().setText("관리자").setCallbackData("관리자"));
-
-            rowsInline.add(rowInline);
-
-            markupInline.setKeyboard(rowsInline);
-            message.setReplyMarkup(markupInline);
             try {
-                execute(message);
+                execute(sendMessage(update));
             } catch (TelegramApiException e) {
                 e.printStackTrace();
             }
         }
         else if(update.hasCallbackQuery()) {
-            String call_data = update.getCallbackQuery().getData();
-            long message_id = update.getCallbackQuery().getMessage().getMessageId();
-            long chat_id = update.getCallbackQuery().getMessage().getChatId();
-
-            EditMessageText new_message = new EditMessageText()
-                    .setChatId(chat_id)
-                    .setMessageId(toIntExact(message_id))
-                    .setText(call_data);
             try {
-                execute(new_message);
+                execute(sendCallBackMessage(update));
             } catch (TelegramApiException e) {
                 e.printStackTrace();
             }
         }
+    }
+
+    private SendMessage sendMessage(Update update) {
+        String text = update.getMessage().getText();
+        System.out.println(text);
+
+        SendMessage message = new SendMessage()
+                .setChatId(update.getMessage().getChatId())
+                .setText(text);
+        InlineKeyboardMarkup markupInline = new InlineKeyboardMarkup();
+
+        List<List<InlineKeyboardButton>> rowsInline = new ArrayList<>();
+        List<InlineKeyboardButton> rowInline = new ArrayList<>();
+
+        rowInline.add(new InlineKeyboardButton().setText("적중").setCallbackData(text + " : 적중"));
+        rowInline.add(new InlineKeyboardButton().setText("관리자").setCallbackData(text + " : 관리자"));
+
+        rowsInline.add(rowInline);
+
+        markupInline.setKeyboard(rowsInline);
+        message.setReplyMarkup(markupInline);
+
+        return message;
+    }
+
+    private EditMessageText sendCallBackMessage(Update update) {
+        String call_data = update.getCallbackQuery().getData();
+        long message_id = update.getCallbackQuery().getMessage().getMessageId();
+        long chat_id = update.getCallbackQuery().getMessage().getChatId();
+
+        EditMessageText new_message = new EditMessageText()
+                .setChatId(chat_id)
+                .setMessageId(toIntExact(message_id))
+                .setText(call_data);
+
+        return new_message;
     }
 
     @Override
