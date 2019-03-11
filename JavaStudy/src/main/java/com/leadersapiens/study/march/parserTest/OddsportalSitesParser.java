a47f20dfb6e75c0b2267f25622d80d7ad654a44f
Berea96 <kim-kj960712@daum.net>
2019-03-11 22:33:26 +0900

    Crawling 부분 생성
    현재 크롤링관련 부분까지 되었다!

--- JavaStudy/src/main/java/com/leadersapiens/study/march/parserTest/OddsportalSitesParser.java
+++ JavaStudy/src/main/java/com/leadersapiens/study/march/parserTest/OddsportalSitesParser.java
@@ -1,5 +1,15 @@
 package com.leadersapiens.study.march.parserTest;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.apache.log4j.Logger;
+
+import java.io.IOException;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -7,8 +17,9 @@ import java.util.TimerTask;
  * oddsportal사이트에서의 배팅사이트 정보를 가져오는 클래스
  */
 public class OddsportalSitesParser extends TimerTask {
+    private static Logger logger = new Logger(OddsportalSitesParser.class.getName());
 
-    private String url = "https://www.oddsportal.com/res/x/bookies-190305102620-1552279100.js";
+    static final String url = "https://www.oddsportal.com/res/x/bookies-190305102620-1552279100.js";
 
     @Override
     public void run() {
@@ -17,10 +28,38 @@ public class OddsportalSitesParser extends TimerTask {
 
     public void oddsportalParser() {
 
+
+        String getBody = getHtmlBody();
     }
 
-    public void getHtmlBody() {
+    public String getHtmlBody() {
 
+        RequestConfig requestConfig = RequestConfig.custom()
+                .setConnectTimeout(10000)
+                .setSocketTimeout(15000).build();
+
+        HttpClient httpClient = HttpClients.custom()
+                .setDefaultRequestConfig(requestConfig)
+                .build();
+
+        String responseString = "";
+
+        HttpGet httpGet = new HttpGet(url);
+
+        HttpResponse httpResponse = null;
+
+        try {
+            httpResponse = httpClient.execute(httpGet);
+
+            HttpEntity httpEntity = httpResponse.getEntity();
+
+            responseString = EntityUtils.toString(httpEntity);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return responseString;
     }
 
     public static void main(String[] args) {
